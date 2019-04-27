06adbd10af340ea1b9df5a261bd1f4b596b7bcf9
Anthony DiRusso <ossurid@gmail.com>
2019-04-27 07:08:45 -0400

    adds Start.js and links it to parent App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import ElapsedTime from './ElapsedTime';
+import Start from './Start';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -15,9 +17,20 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <ElapsedTime sessionElapsedTime={this.state.sessionElapsedTime} hours={this.state.hours} minutes={this.state.minutes} seconds={this.state.seconds} />
+          <Start handler={this._startButton} />
         </header>
       </div>
     );
+  }
+  _startButton = () => {
+    this.setState({
+      startTime: this.state.totalElapsedTime,
+      sessionElapsedTime: 0,
+      stopWatchOn: true,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    })
   }
 }
 
---
+++ src/Start.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Start(props) {
+  return (
+    <button onClick={props.handler}>Start</button>
+  )
+}
+
+export default Start;
