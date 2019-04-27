7eee358bfc09a76db3ca0ba60dfab0ab1dc247a7
Anthony DiRusso <ossurid@gmail.com>
2019-04-27 07:12:31 -0400

    adds stop file and links to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import ElapsedTime from './ElapsedTime';
 import Start from './Start';
+import Stop from './Stop';
+
 
 
 class App extends React.Component {
@@ -18,6 +20,7 @@ class App extends React.Component {
         <header className="App-header">
           <ElapsedTime sessionElapsedTime={this.state.sessionElapsedTime} hours={this.state.hours} minutes={this.state.minutes} seconds={this.state.seconds} />
           <Start handler={this._startButton} />
+          <Stop handler={this._stopButton} />
         </header>
       </div>
     );
@@ -30,6 +33,12 @@ class App extends React.Component {
       hours: 0,
       minutes: 0,
       seconds: 0
+    })
+  }
+  _stopButton = () => {
+    this.setState({
+      startTime: 0,
+      stopWatchOn: false
     })
   }
 }
---
+++ src/Stop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Stop(props) {
+  return (
+    <button onClick={props.handler}>Stop</button>
+  )
+}
+
+export default Stop;
